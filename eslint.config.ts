9c10079a67cd4ba024ad04f5e3ce6ae70ab74d65
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2025-04-14 21:40:02 +0600

    update deps

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,7 +2,7 @@ import createConfig from "@antfu/eslint-config";
 import tanstackRouterPlugin from "@tanstack/eslint-plugin-router";
 
 export default createConfig({
-  ignores: ["./backend/", ".env.json"],
+  ignores: ["./backend/", ".env.json", "**/routeTree.gen.ts"],
   stylistic: {
     quotes: "double",
     semi: true,
