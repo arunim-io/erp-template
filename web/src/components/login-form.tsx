3fd7ac7fd27d00be37b512d952d2cfe0c3f610d2
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2025-04-15 23:05:37 +0600

    add login page

---
+++ web/src/components/login-form.tsx
@@ -0,0 +1,59 @@
+import { Button } from "$src/components/ui/button";
+import { Input } from "$src/components/ui/input";
+import { Label } from "$src/components/ui/label";
+import { cn } from "$src/lib/utils";
+import { SiFacebook, SiGoogle } from "@icons-pack/react-simple-icons";
+
+export function LoginForm({ className, ...props }: React.ComponentProps<"form">) {
+  return (
+    <form className={cn("flex flex-col gap-6", className)} {...props}>
+      <div className="flex flex-col items-center gap-2 text-center">
+        <h1 className="text-2xl font-bold">Login to your account</h1>
+        <p className="text-sm text-balance text-muted-foreground">
+          Enter your email below to login to your account
+        </p>
+      </div>
+      <div className="grid gap-6">
+        <div className="grid gap-3">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" type="email" placeholder="m@example.com" required />
+        </div>
+        <div className="grid gap-3">
+          <div className="flex items-center">
+            <Label htmlFor="password">Password</Label>
+            <a
+              href="#"
+              className="ml-auto text-sm underline-offset-4 hover:underline"
+            >
+              Forgot your password?
+            </a>
+          </div>
+          <Input id="password" type="password" placeholder="**********" required />
+        </div>
+        <Button type="submit" className="w-full">
+          Login
+        </Button>
+        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+          <span className="relative z-10 bg-background px-2 text-muted-foreground">
+            Or continue with
+          </span>
+        </div>
+        <Button variant="outline" className="w-full">
+          <SiGoogle />
+          Login with Google
+        </Button>
+        <Button variant="outline" className="w-full">
+          <SiFacebook />
+          Login with Facebook
+        </Button>
+      </div>
+      <div className="text-center text-sm">
+        Don&apos;t have an account?
+        {" "}
+        <a href="#" className="underline underline-offset-4">
+          Sign up
+        </a>
+      </div>
+    </form>
+  );
+}
