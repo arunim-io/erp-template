796330f21e205247a418f17faa79d93a57d67deb
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2025-04-28 23:51:43 +0600

    update eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,11 @@
-import antfu from '@antfu/eslint-config'
+import antfu from "@antfu/eslint-config";
 
 export default antfu({
+  stylistic: {
+    quotes: "double",
+    semi: true,
+  },
   formatters: true,
   react: true,
-})
+  toml: false,
+});
