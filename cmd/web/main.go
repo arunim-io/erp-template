9050059f51704382ab7627a9d87d7715eb7c7077
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2025-09-13 17:57:03 +0600

    Replace `main.go` with a new web server implementation in `cmd/web/main.go`, featuring improved logging and graceful shutdown handling.

---
+++ cmd/web/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	s := http.Server{
+		Addr:         net.JoinHostPort("0.0.0.0", "8000"),
+		Handler:      mux,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	go func() {
+		logger.Info("Starting server", "address", s.Addr)
+
+		if err := s.ListenAndServe(); err != nil && !errors.Is(http.ErrServerClosed, err) {
+			logger.Error("Failed to start server", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	quit_chan := make(chan os.Signal, 1)
+	signal.Notify(quit_chan, syscall.SIGINT, syscall.SIGTERM)
+	<-quit_chan
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := s.Shutdown(ctx); err != nil {
+		logger.Error("Server forced to shut down", "error", err)
+		os.Exit(1)
+	}
+
+	logger.Info("Server has exited...")
+}
