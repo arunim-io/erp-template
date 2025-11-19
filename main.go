9c9f0253a60e095ce23b70cd27840af1e6e1013a
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2025-11-19 12:41:08 +0600

    configure http server

--- main.go
+++ main.go
@@ -1,17 +1,55 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log/slog"
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
 	"time"
 )
 
-func greet(w http.ResponseWriter, r *http.Request) {
+var greet http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) { //nolint:revive // Just an useless warning
 	_, _ = fmt.Fprintf(w, "Hello World! %s", time.Now())
 }
 
 func main() {
-	http.HandleFunc("/", greet)
-	fmt.Println("Started server at :8000")
-	_ = http.ListenAndServe(":8080", nil)
+	const timeout = 5 * time.Second
+
+	log := slog.New(slog.NewTextHandler(os.Stderr, nil))
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", greet)
+
+	addr := net.JoinHostPort("localhost", "8000")
+	server := &http.Server{
+		Addr:              addr,
+		Handler:           mux,
+		ReadHeaderTimeout: timeout,
+	}
+
+	go func() {
+		log.Info("Started server...", "addr", addr)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Error("Couldn't listen: %v\n", "error", err)
+		}
+	}()
+
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, os.Interrupt)
+
+	<-stopChan
+	log.Info("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Error("Server forced to shutdown", "error", err)
+	}
+
+	log.Info("Server has successfully shut down.")
 }
